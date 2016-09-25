556d3185a30f790cdb1b997bf2ff6f513fbbb21b
ligi <ligi@ligi.de>
2016-09-25 03:28:03 +0200

    Use FalconSpoon

--- android/src/androidTest/java/org/ligi/survivalmanual/ScreenShotTaker.java
+++ android/src/androidTest/java/org/ligi/survivalmanual/ScreenShotTaker.java
@@ -1,12 +1,12 @@
 package org.ligi.survivalmanual;
 
-import com.squareup.spoon.Spoon;
+import com.jraska.falcon.FalconSpoon;
 
 class ScreenShotTaker {
 
     static void takeScreenShot(final MainActivity activity, final String help) {
         try {
-            Spoon.screenshot(activity, help);
+            FalconSpoon.screenshot(activity, help);
         } catch (Exception ignored) {
             // we could not take the screenshot - no big deal
         }
