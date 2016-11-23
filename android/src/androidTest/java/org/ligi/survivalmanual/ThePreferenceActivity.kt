8efdcf1d57646cbf7a24dcdb46df4b504692fc5b
ligi <ligi@ligi.de>
2016-11-23 22:47:42 +0100

    Use trulesk

--- android/src/androidTest/java/org/ligi/survivalmanual/ThePreferenceActivity.kt
+++ android/src/androidTest/java/org/ligi/survivalmanual/ThePreferenceActivity.kt
@@ -1,32 +1,19 @@
 package org.ligi.survivalmanual
 
-import android.support.test.InstrumentationRegistry
-import android.support.test.espresso.Espresso
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.action.ViewActions.click
 import android.support.test.espresso.matcher.ViewMatchers.withText
-import android.support.test.rule.ActivityTestRule
 import android.support.v7.app.AppCompatDelegate
-import android.view.WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD
-import android.view.WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.ligi.passandroid.reporting.SpooningFailureHandler
-import org.ligi.survivalmanual.ScreenShotTaker.takeScreenShot
+import org.ligi.trulesk.TruleskActivityRule
 
 
 class ThePreferenceActivity {
 
     @get:Rule
-    val activityTestRule = ActivityTestRule(PreferenceActivity::class.java)
-
-    @Before
-    fun setUp() {
-        Espresso.setFailureHandler (SpooningFailureHandler(InstrumentationRegistry.getInstrumentation()))
-        activityTestRule.activity.runOnUiThread { activityTestRule.activity.window.addFlags(FLAG_TURN_SCREEN_ON or FLAG_DISMISS_KEYGUARD) }
-    }
+    val activityTestRule = TruleskActivityRule(PreferenceActivity::class.java)
 
     @Test
     fun thatDayNightAutoIsSelectable() {
@@ -37,9 +24,8 @@ class ThePreferenceActivity {
 
         assertThat(AppCompatDelegate.getDefaultNightMode()).isEqualTo(AppCompatDelegate.MODE_NIGHT_AUTO)
         assertThat(State.getNightMode()).isEqualTo(AppCompatDelegate.MODE_NIGHT_AUTO)
-        takeScreenShot(activityTestRule.activity, "daynight_auto")
+        activityTestRule.screenShot("daynight_auto")
     }
-
 
     @Test
     fun thatDayIsSelectable() {
@@ -50,9 +36,8 @@ class ThePreferenceActivity {
 
         assertThat(AppCompatDelegate.getDefaultNightMode()).isEqualTo(AppCompatDelegate.MODE_NIGHT_NO)
         assertThat(State.getNightMode()).isEqualTo(AppCompatDelegate.MODE_NIGHT_NO)
-        takeScreenShot(activityTestRule.activity, "daynight_day")
+        activityTestRule.screenShot("daynight_day")
     }
-
 
     @Test
     fun thatNightIsSelectable() {
@@ -63,7 +48,7 @@ class ThePreferenceActivity {
 
         assertThat(AppCompatDelegate.getDefaultNightMode()).isEqualTo(AppCompatDelegate.MODE_NIGHT_YES)
         assertThat(State.getNightMode()).isEqualTo(AppCompatDelegate.MODE_NIGHT_YES)
-        takeScreenShot(activityTestRule.activity, "daynight_night")
+        activityTestRule.screenShot("daynight_night")
     }
 
     @Test
--- android/src/androidTest/java/org/ligi/survivalmanual/TheSurvivalActivity.kt
+++ android/src/androidTest/java/org/ligi/survivalmanual/TheSurvivalActivity.kt
@@ -1,35 +1,21 @@
 package org.ligi.survivalmanual
 
 import android.os.SystemClock
-import android.support.test.InstrumentationRegistry
-import android.support.test.espresso.Espresso
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.contrib.DrawerActions
 import android.support.test.espresso.contrib.NavigationViewActions.navigateTo
 import android.support.test.espresso.matcher.ViewMatchers.*
-import android.support.test.rule.ActivityTestRule
-import android.view.WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD
-import android.view.WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
 import org.assertj.core.api.Assertions.assertThat
 import org.hamcrest.Matchers.containsString
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.ligi.passandroid.reporting.SpooningFailureHandler
-import org.ligi.survivalmanual.ScreenShotTaker.takeScreenShot
-
+import org.ligi.trulesk.TruleskActivityRule
 
 class TheSurvivalActivity {
 
     @get:Rule
-    val activityTestRule = ActivityTestRule(MainActivity::class.java)
-
-    @Before
-    fun setUp() {
-        Espresso.setFailureHandler (SpooningFailureHandler(InstrumentationRegistry.getInstrumentation()))
-        activityTestRule.activity.runOnUiThread { activityTestRule.activity.window.addFlags(FLAG_TURN_SCREEN_ON or FLAG_DISMISS_KEYGUARD) }
-    }
+    val activityTestRule = TruleskActivityRule(MainActivity::class.java)
 
     @Test
     fun thatActivityShouldLaunch() {
@@ -38,7 +24,7 @@ class TheSurvivalActivity {
 
     @Test
     fun thatThatHelpOpens() {
-        TestHelper.invokeMenu(R.id.menu_help,R.string.introduction)
+        TestHelper.invokeMenu(R.id.menu_help, R.string.introduction)
 
         onView(withText(R.string.help_title)).check(matches(isDisplayed()))
     }
@@ -50,7 +36,7 @@ class TheSurvivalActivity {
 
         onView(withText(containsString(BuildConfig.VERSION_NAME))).check(matches(isDisplayed()))
 
-        takeScreenShot(activityTestRule.activity, "help")
+        activityTestRule.screenShot("help")
     }
 
     /* TODO remove the need for the sleeps - but without the test was flaky */
@@ -66,7 +52,7 @@ class TheSurvivalActivity {
             val subtitle = activity.supportActionBar!!.subtitle
             assertThat(subtitle).isEqualTo(activity.getString(NavigationDefinitions.getTitleResByURL(url)))
 
-            takeScreenShot(activity, "topic_" + subtitle!!.toString().replace(" ", "_").replace("/", "_"))
+            activityTestRule.screenShot("topic_" + subtitle!!.toString().replace(" ", "_").replace("/", "_"))
         }
     }
 
