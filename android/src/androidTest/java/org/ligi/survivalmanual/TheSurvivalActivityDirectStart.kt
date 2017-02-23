624161213929c78bcb7153e158dc8a13a786fdd1
ligi <ligi@ligi.de>
2017-02-23 22:31:36 +0100

    Remove the tests for help as help is not there this way anymore Now part of content

--- android/src/androidTest/java/org/ligi/survivalmanual/TheSurvivalActivityDirectStart.kt
+++ android/src/androidTest/java/org/ligi/survivalmanual/TheSurvivalActivityDirectStart.kt
@@ -2,20 +2,16 @@ package org.ligi.survivalmanual
 
 import android.os.SystemClock
 import android.support.test.espresso.Espresso.onView
-import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.contrib.DrawerActions
 import android.support.test.espresso.contrib.NavigationViewActions.navigateTo
-import android.support.test.espresso.matcher.ViewMatchers.*
+import android.support.test.espresso.matcher.ViewMatchers.withId
 import org.assertj.core.api.Assertions.assertThat
-
-import org.hamcrest.Matchers.containsString
 import org.junit.Rule
 import org.junit.Test
 import org.ligi.survivalmanual.model.NavigationEntryMap
 import org.ligi.survivalmanual.model.titleResByURLMap
 import org.ligi.survivalmanual.ui.MainActivity
 import org.ligi.trulesk.TruleskActivityRule
-import org.ligi.trulesk.invokeMenu
 
 class TheSurvivalActivityDirectStart {
 
@@ -24,23 +20,6 @@ class TheSurvivalActivityDirectStart {
 
     @Test
     fun thatActivityShouldLaunch() {
-    }
-
-    @Test
-    fun thatThatHelpOpens() {
-        invokeMenu(R.id.menu_help, R.string.introduction)
-
-        onView(withText(R.string.help_title)).check(matches(isDisplayed()))
-    }
-
-
-    @Test
-    fun testThatHelpContainsVersion() {
-        thatThatHelpOpens()
-
-        onView(withText(containsString(BuildConfig.VERSION_NAME))).check(matches(isDisplayed()))
-
-        activityTestRule.screenShot("help")
     }
 
     /* TODO remove the need for the sleeps - but without the test was flaky */
