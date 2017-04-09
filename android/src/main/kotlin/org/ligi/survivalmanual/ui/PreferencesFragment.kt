33dbfc0f3e69aeaab389e6f6290e6abb71a3c6ab
ligi <ligi@ligi.de>
2017-04-10 00:25:53 +0200

    Convert class to kotlin

---
+++ android/src/main/kotlin/org/ligi/survivalmanual/ui/PreferencesFragment.kt
@@ -0,0 +1,56 @@
+package org.ligi.survivalmanual.ui
+
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.support.v7.app.AppCompatDelegate
+import android.support.v7.app.AppCompatDelegate.MODE_NIGHT_AUTO
+import android.support.v7.preference.PreferenceFragmentCompat
+import org.ligi.kaxt.recreateWhenPossible
+import org.ligi.survivalmanual.R
+import org.ligi.survivalmanual.model.State
+import permissions.dispatcher.NeedsPermission
+import permissions.dispatcher.RuntimePermissions
+
+@RuntimePermissions
+class PreferencesFragment : PreferenceFragmentCompat(), SharedPreferences.OnSharedPreferenceChangeListener {
+
+    override fun onResume() {
+        super.onResume()
+        preferenceScreen.sharedPreferences.registerOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        preferenceScreen.sharedPreferences.unregisterOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, key: String) {
+        if (key == getString(R.string.preference_key_nightmode)) {
+
+            @AppCompatDelegate.NightMode val nightMode = State.getNightMode()
+
+            if (nightMode == MODE_NIGHT_AUTO) {
+                PreferencesFragmentPermissionsDispatcher.ensureDayNightWithCheck(this)
+            }
+
+            AppCompatDelegate.setDefaultNightMode(nightMode)
+            activity.recreateWhenPossible()
+        }
+    }
+
+
+    override fun onCreatePreferences(bundle: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences, rootKey)
+    }
+
+    @NeedsPermission(android.Manifest.permission.ACCESS_COARSE_LOCATION)
+    fun ensureDayNight() {
+        // Intentionally empty
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        PreferencesFragmentPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults)
+    }
+
+}
