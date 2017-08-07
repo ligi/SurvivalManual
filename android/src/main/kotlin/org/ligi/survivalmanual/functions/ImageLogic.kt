ceff4394116e0fea9ef31880f2b1b2507c882875
ligi <ligi@ligi.de>
2017-08-07 20:43:35 +0200

    Also match images that have no description

--- android/src/main/kotlin/org/ligi/survivalmanual/functions/ImageLogic.kt
+++ android/src/main/kotlin/org/ligi/survivalmanual/functions/ImageLogic.kt
@@ -1,7 +1,7 @@
 package org.ligi.survivalmanual.functions
 
 fun linkImagesInMarkDown(markdown: String): String {
-    return markdown.replace(Regex("!\\[([^\\]]+)\\]\\(([^)]+)\\)"), { matchResult: MatchResult ->
+    return markdown.replace(Regex("!\\[([^\\]]*)\\]\\(([^)]+)\\)"), { matchResult: MatchResult ->
         "[" + matchResult.value + "](" + matchResult.groupValues[2] + ")"
     })
 }
