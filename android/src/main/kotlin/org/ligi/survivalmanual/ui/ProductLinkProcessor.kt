5192a708fb5be875490b39a47c3db99ec7ea40f1
ligi <ligi@ligi.de>
2021-01-12 05:14:45 +0100

    Fix some links

--- android/src/main/kotlin/org/ligi/survivalmanual/ui/ProductLinkProcessor.kt
+++ android/src/main/kotlin/org/ligi/survivalmanual/ui/ProductLinkProcessor.kt
@@ -9,12 +9,12 @@ import org.ligi.kaxt.startActivityFromURL
 import org.ligi.survivalmanual.R
 
 val PRODUCT_MAP = mapOf(
-        "SolarUSBCharger" to "B012YUJJM8/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B012YUJJM8&linkCode=as2&tag=ligi-20&linkId=02d3fbda3eaadbd10744c42805e0e791",
+        "SolarUSBCharger" to "B01EXWCPLC/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B01EXWCPLC&linkCode=as2&tag=ligi-20&linkId=91a10115f5e5cfae9b2529ca1a96c074",
         "CampStoveUSB" to "B00BQHET9O/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B00BQHET9O&linkCode=as2&tag=ligi-20&linkId=d949a1aca04d67b5e61d3bf77ce89d22",
-        "HandCrankUSB" to "B01AD7IN4O/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B01AD7IN4O&linkCode=as2&tag=ligi-20&linkId=9a9c9d7ff318d594d077fa917f8c3739",
+        "HandCrankUSB" to "B08D9KLNV3/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B08D9KLNV3&linkCode=as2&tag=ligi-20&linkId=2f8f83d3bb8ec615be3f8849abf751c2",
         "CarUSBCharger" to "B00VH84L5E/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B00VH84L5E&linkCode=as2&tag=ligi-20&linkId=41a56b9c800ed019a0af367a49050502",
-        "OHTMultiTool" to "B008P8EYWE/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B008P8EYWE&linkCode=as2&tag=ligi-20&linkId=e72720183453da1b74c2a0413521b194",
-        "LifeStraw" to "B006QF3TW4/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B006QF3TW4&linkCode=as2&tag=ligi-20&linkId=5b949343cb5a5f03b220651ad6830a9d",
+        "OHTMultiTool" to "B008069YXA/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B008069YXA&linkCode=as2&tag=ligi-20&linkId=a286d415f362e4650652f63a4dae9a3e",
+        "LifeStraw" to "B006QF3TW4/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B006QF3TW4&linkCode=as2&tag=ligi-20&linkId=140954cab61f21b92f27217172f3ec35",
         "TreadMultiTool" to "B018IOXXP8/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B018IOXXP8&linkCode=as2&tag=ligi-20&linkId=1c14c69653606e308b9f4b98372a5a51",
         "PandaDubLionsDen" to "B00F48QJUS/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B00F48QJUS&linkCode=as2&tag=ligi-20&linkId=3e9b42f0dee5e03da96ea1f14be223ea",
         "Audible" to "B00NB86OYE/?ref_=assoc_tag_ph_1485906643682&_encoding=UTF8&camp=1789&creative=9325&linkCode=pf4&tag=ligi-20&linkId=38dacab0a96e0ecaa625fa375f70c517"
