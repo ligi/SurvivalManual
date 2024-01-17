7b2adde0f022572e88f1cc1375a9b545a3629d5e
Dennis Guse <dennis.guse@alumni.tu-berlin.de>
2024-01-17 07:50:40 +0100

    Settings: remove night mode auto as follow system works.
    Night mode auto requires location permissions.

--- android/src/main/kotlin/org/ligi/survivalmanual/ui/PreferencesFragment.kt
+++ android/src/main/kotlin/org/ligi/survivalmanual/ui/PreferencesFragment.kt
@@ -7,30 +7,23 @@ import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_AUTO
 import androidx.preference.PreferenceFragmentCompat
 import org.ligi.survivalmanual.R
 import org.ligi.survivalmanual.model.State
-import permissions.dispatcher.NeedsPermission
-import permissions.dispatcher.RuntimePermissions
 
-@RuntimePermissions
 class PreferencesFragment : PreferenceFragmentCompat(), SharedPreferences.OnSharedPreferenceChangeListener {
 
     override fun onResume() {
         super.onResume()
-        preferenceScreen.sharedPreferences.registerOnSharedPreferenceChangeListener(this)
+        preferenceScreen.sharedPreferences?.registerOnSharedPreferenceChangeListener(this)
     }
 
     override fun onPause() {
         super.onPause()
-        preferenceScreen.sharedPreferences.unregisterOnSharedPreferenceChangeListener(this)
+        preferenceScreen.sharedPreferences?.unregisterOnSharedPreferenceChangeListener(this)
     }
 
-    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, key: String) {
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
         if (key == getString(R.string.preference_key_nightmode)) {
 
             @AppCompatDelegate.NightMode val nightMode = State.getNightMode()
-
-            if (nightMode == MODE_NIGHT_AUTO) {
-                ensureDayNightWithPermissionCheck()
-            }
 
             AppCompatDelegate.setDefaultNightMode(nightMode)
             activity?.recreate()
@@ -40,16 +33,6 @@ class PreferencesFragment : PreferenceFragmentCompat(), SharedPreferences.OnShar
 
     override fun onCreatePreferences(bundle: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
-    }
-
-    @NeedsPermission(android.Manifest.permission.ACCESS_COARSE_LOCATION)
-    fun ensureDayNight() {
-        // Intentionally empty
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        onRequestPermissionsResult(requestCode, grantResults)
     }
 
 }
