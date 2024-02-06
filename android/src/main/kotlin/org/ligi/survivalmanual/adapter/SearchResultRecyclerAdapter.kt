faf4fb88584cb6a16d6b310faa0b8b196c3977c7
Dennis Guse <dennis.guse@alumni.tu-berlin.de>
2024-02-06 08:11:14 +0100

    Cleanup: remove ExtraCompat.

--- android/src/main/kotlin/org/ligi/survivalmanual/adapter/SearchResultRecyclerAdapter.kt
+++ android/src/main/kotlin/org/ligi/survivalmanual/adapter/SearchResultRecyclerAdapter.kt
@@ -2,8 +2,8 @@ package org.ligi.survivalmanual.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.text.HtmlCompat
 import androidx.recyclerview.widget.RecyclerView
-import org.ligi.compat.HtmlCompat
 import org.ligi.survivalmanual.R
 import org.ligi.survivalmanual.functions.highLight
 import org.ligi.survivalmanual.functions.search
@@ -34,7 +34,7 @@ class SearchResultRecyclerAdapter(private var term: String,
             holder.titleTextView.text = holder.view.context.getString(title)
         }
 
-        holder.teaserTextView.text = HtmlCompat.fromHtml(highLight(list[position].teaser, term))
+        holder.teaserTextView.text = HtmlCompat.fromHtml(highLight(list[position].teaser, term), HtmlCompat.FROM_HTML_MODE_LEGACY)
         holder.itemView.setOnClickListener {
             onClick.invoke(list[holder.adapterPosition].file)
         }
