86443cd624c667b039bc2aaa415b78504f130a5f
Dennis Guse <dennis.guse@alumni.tu-berlin.de>
2024-02-06 08:06:18 +0100

    Cleanup: remove TraceDroid.

--- android/src/main/kotlin/org/ligi/survivalmanual/App.kt
+++ android/src/main/kotlin/org/ligi/survivalmanual/App.kt
@@ -2,13 +2,11 @@ package org.ligi.survivalmanual
 
 import android.app.Application
 import org.ligi.survivalmanual.model.State
-import org.ligi.tracedroid.TraceDroid
 
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        TraceDroid.init(this)
         State.applyDayNightMode()
     }
 }
